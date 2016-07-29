ddb8103a7ac606eb24b08061b0634830f0127776
Wiktor <wiktorwrosz@o2.pl>
2016-07-29 18:36:00 +0200

    1

--- Unit1.cpp
+++ Unit1.cpp
@@ -4,6 +4,8 @@
 #pragma hdrstop
 
 #include "Unit1.h"
+#include<iostream>
+#include<fstream.h>
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -14,4 +16,14 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+        const int n=5;
+        double x[n]={1.2, .357, 0., 100000, 0.5}, y[n];
+        ofstream liczby("c:\\liczby.txt");
+        liczby.write((char*) x,n*sizeof(double));
+        liczby.close();
+}
+//---------------------------------------------------------------------------
+
--- Unit1.h
+++ Unit1.h
@@ -7,12 +7,15 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <jpeg.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
         TButton *Button1;
-        TLabel *Label1;
+        TImage *Image1;
+        void __fastcall Button1Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
