b35fb4be4d2e7fa7b3dcd7da35d9faf87fc6bf9a
Wiktor <wiktorwrosz@o2.pl>
2016-08-04 19:20:06 +0200

    dodano unit2 i unit3 do repozytorium

---
+++ Unit3.h
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit3H
+#define Unit3H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm3 : public TForm
+{
+__published:	// IDE-managed Components
+        TLabel *Label1;
+        TButton *Button2;
+        void __fastcall Button2Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm3(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm3 *Form3;
+//---------------------------------------------------------------------------
+#endif
